c050a4dc842da14ba6380e553139dcd57fd2b737
louie-cai <louiecai@outlook.com>
2022-02-04 12:19:13 -0800

    main script renamed & implemented with logo printing and call to main_scraper.py

---
+++ raspi-scraper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from utils import print_logo
+from main_scraper import scrape
+
+
+def main():
+    print_logo()
+    scrape('./sources.json')
+    print()
+
+if __name__ == '__main__':
+    main()
